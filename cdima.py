ffa2b8ed4880d2b5c63989caa9624b9c29b9879b
Ines <ideremedios.ieu2023@student.ie.edu>
2025-01-29 21:41:15 +0100

    cdima model

--- cdima.py
+++ cdima.py
@@ -2,6 +2,8 @@ import numpy as np
 import scipy as sp
 from scipy.optimize import fsolve
 from scipy.integrate import solve_ivp
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
 
 
 def cdima(a, b, x, y):
@@ -20,15 +22,6 @@ def rk4_cdima(a, b, x0, y0, t_span, t_eval):
     return sol
 
 
-a = 10
-b = 6
-t_span = (0, 100)
-t_eval = np.linspace(0, 10, 100)
-x0, y0 = 0, 0
-solution = rk4_cdima(a, b, x0, y0, t_span, t_eval)
-print(solution)
-
-
 def find_root(a, b):
     def equations(vars):
         x, y = vars
@@ -39,8 +32,31 @@ def find_root(a, b):
     return root
 
 
-a = 1.0
-b = 2.0
+def trajectory(a, b, x0, y0, t_span, t_eval):
+    solution = rk4_cdima(a, b, x0, y0, t_span, t_eval)
+    return solution.t, solution.y[0], solution.y[1]
+
+
+def nullclines(a, b, x_range):
+    x = np.linspace(x_range[0], x_range[1], 100)
+    # dxdt=0
+    nullcline1 = (a - x) - ((1 + x**2) / (4 * x))
+    # dydt=0
+    nullcline2 = (1 + x**2) * np.ones_like(
+        x
+    )  # x=0 is trivial, bu then for any value of x, the value of y on the nullcline is 1+ x**2
+    return x, nullcline1, nullcline2
+
+
+a = 10
+b = 6
+t_span = (0, 100)
+t_eval = np.linspace(0, 10, 100)
+x0, y0 = 0, 0
+t, x, y = trajectory(a, b, x0, y0, t_span, t_eval)
+x_range = (0, 5)
+solution = rk4_cdima(a, b, x0, y0, t_span, t_eval)
 r = find_root(a, b)
 root = tuple(r)
+print(solution)
 print(root)
